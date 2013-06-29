082df15d696da46db177acf0ed2128a2d4fe1292
Richard Thai <rthai@groupon.com>
2013-06-29 12:39:00 -0700

    code refactoring

--- src/com/example/shopping_list/ListsActivity.java
+++ src/com/example/shopping_list/ListsActivity.java
@@ -39,54 +39,58 @@ public class ListsActivity extends Activity {
         final EditText addItemEditText = (EditText) findViewById(R.id.add_item_edit_text);
         final EditText addListEditText = (EditText) findViewById(R.id.add_list_edit_text);
 
-        // The item is added from th edit text.
-        addItemEditText.setImeActionLabel("Add Item", KeyEvent.KEYCODE_ENTER);
-        addItemEditText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
-            @Override
-            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
-                //get the text
-                String name = addItemEditText.getText().toString();
-
-
-                //create an item based on the text
-                Item newItem = new Item();
-                newItem.name = name;
-                newItem.listId = mdbHelper.getList(currentListOrder).id;
-
-                mdbHelper.addItem(newItem);
-                mDataAdapter.notifyDataSetChanged();
-                return true;
+        addItemEditText.setFocusableInTouchMode(true);
+        addItemEditText.setOnKeyListener(new View.OnKeyListener() {
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                // If the event is a key-down event on the "enter" button
+                if ((event.getAction() == KeyEvent.ACTION_DOWN) &&
+                        (keyCode == KeyEvent.KEYCODE_ENTER)) {
+                    addItem(addItemEditText);
+                    return true;
+                }
+                return false;
             }
         });
 
-        // set edit text listener
-        addListEditText.setImeActionLabel("Add List", KeyEvent.KEYCODE_ENTER);
-        addListEditText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
-            @Override
-            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
-                //get the text
-                String title = addListEditText.getText().toString();
-
-
-                //create a list based on the text
-                ShoppingList newList = new ShoppingList();
-                newList.title = title;
-
-                mdbHelper.addList(newList);
-
-                return true;
+        addListEditText.setFocusableInTouchMode(true);
+        addListEditText.setOnKeyListener(new View.OnKeyListener() {
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                // If the event is a key-down event on the "enter" button
+                if ((event.getAction() == KeyEvent.ACTION_DOWN) &&
+                        (keyCode == KeyEvent.KEYCODE_ENTER)) {
+                    addList(addItemEditText);
+                    return true;
+                }
+                return false;
             }
         });
-
-//        final ArrayList<String> list = new ArrayList<String>();
-//        for (int i = 0; i < values.length; ++i) {
-//            list.add(values[i]);
-//        }
 
         //Generate ListView from SQLite Database
         displayListView(currentListOrder);
+    }
 
+    private void addList(EditText addListEditText) {
+        //get the text
+        String title = addListEditText.getText().toString();
 
+        //create a list based on the text
+        ShoppingList newList = new ShoppingList();
+        newList.title = title;
+
+        mdbHelper.addList(newList);
+    }
+
+    private void addItem(EditText addItemEditText) {
+        //get the text
+        String name = addItemEditText.getText().toString();
+
+        //create an item based on the text
+        Item newItem = new Item();
+        newItem.name = name;
+        newItem.listId = mdbHelper.getList(currentListOrder).id;
+
+        mdbHelper.addItem(newItem);
+        mDataAdapter.notifyDataSetChanged();
     }
 
     private void displayListView(int listOrder) {
@@ -125,41 +129,7 @@ public class ListsActivity extends Activity {
                                     int position, long id) {
                 // Get the cursor, positioned to the corresponding row in the result set
                 Cursor cursor = (Cursor) parent.getItemAtPosition(position);
-
-                // get whether the item is bought
-                Item item = new Item();
-                item.id = cursor.getInt(cursor.getColumnIndexOrThrow(DBConstants.ItemsCols._ID));
-                item.name = cursor.getString(cursor.getColumnIndexOrThrow(DBConstants.ItemsCols.NAME));
-                item.status = cursor.getInt(cursor.getColumnIndexOrThrow(DBConstants.ItemsCols.STATUS));
-                item.order = cursor.getInt(cursor.getColumnIndexOrThrow(DBConstants.ItemsCols.ITEM_ORDER));
-                item.listId = cursor.getInt(cursor.getColumnIndexOrThrow(DBConstants.ItemsCols._SHOPPING_LIST_ID));
-
-                // animate so that it turns visible/fades away
-                if (item.status == Item.IS_BOUGHT) {
-                    // make it unbought
-                    item.status = Item.UNBOUGHT;
-
-                    view.animate().setDuration(2000).alpha((float) 0)
-                            .withEndAction(new Runnable() {
-                                @Override
-                                public void run() {
-                                    mDataAdapter.notifyDataSetChanged();
-                                    view.setAlpha((float) 0);
-                                }
-                            });
-                } else {
-                    // make it bought
-                    item.status = Item.IS_BOUGHT;
-
-                    view.animate().setDuration(2000).alpha((float) 0.5)
-                            .withEndAction(new Runnable() {
-                                @Override
-                                public void run() {
-                                    mDataAdapter.notifyDataSetChanged();
-                                    view.setAlpha((float) 0.5);
-                                }
-                            });
-                }
+                Item item = toggleBought(view, cursor);
 
                 // update the DB
                 mdbHelper.updateItem(item);
@@ -171,4 +141,46 @@ public class ListsActivity extends Activity {
         listView.setAdapter(mDataAdapter);
     }
 
+    private Item toggleBought(final View view, Cursor cursor) {
+        // get whether the item is bought
+        Item item = new Item();
+        item.id = cursor.getInt(cursor.getColumnIndexOrThrow(DBConstants.ItemsCols._ID));
+        item.name = cursor.getString(cursor.getColumnIndexOrThrow(DBConstants.ItemsCols.NAME));
+        item.status = cursor.getInt(cursor.getColumnIndexOrThrow(DBConstants.ItemsCols.STATUS));
+        item.order = cursor.getInt(cursor.getColumnIndexOrThrow(DBConstants.ItemsCols.ITEM_ORDER));
+        item.listId = cursor.getInt(cursor.getColumnIndexOrThrow(DBConstants.ItemsCols._SHOPPING_LIST_ID));
+
+        // animate so that it turns visible/fades away
+        if (item.status == Item.IS_BOUGHT) {
+            // make it unbought
+            item.status = Item.UNBOUGHT;
+
+            view.animate().setDuration(2000).alpha((float) 0)
+                    .withEndAction(new Runnable() {
+                        @Override
+                        public void run() {
+                            mDataAdapter.notifyDataSetChanged();
+                            view.setAlpha((float) 0);
+                        }
+                    });
+        } else {
+            // make it bought
+            item.status = Item.IS_BOUGHT;
+
+            view.animate().setDuration(2000).alpha((float) 0.5)
+                    .withEndAction(new Runnable() {
+                        @Override
+                        public void run() {
+                            mDataAdapter.notifyDataSetChanged();
+                            view.setAlpha((float) 0.5);
+                        }
+                    });
+        }
+        return item;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
 }
