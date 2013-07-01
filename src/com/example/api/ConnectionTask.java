8d904f2c7b20b85daea90fb1bdc487264824e50b
Snow Zhao <zhaosriuo@sina.com>
2013-07-01 10:05:36 -0700

    updated ConnectionTask class

--- src/com/example/api/ConnectionTask.java
+++ src/com/example/api/ConnectionTask.java
@@ -10,6 +10,14 @@ import java.net.URL;
 
 /**
  * Created with IntelliJ IDEA.
+ * User: sizhao
+ * Date: 6/29/13
+ * Time: 7:46 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+/**
+ * Created with IntelliJ IDEA.
  * User: rthai
  * Date: 6/26/13
  * Time: 3:49 PM
--- src/com/example/shopping_list/ChooseLists.java
+++ src/com/example/shopping_list/ChooseLists.java
@@ -3,8 +3,10 @@ package com.example.shopping_list;
 import android.app.Activity;
 import android.database.Cursor;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.*;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
 import com.example.database.DBConstants;
 import com.example.database.SQLiteHelper;
 import com.example.model.ShoppingList;
@@ -57,23 +59,63 @@ public class ChooseLists extends Activity {
                 to);
 
         ArrayList<ShoppingList> listArray = new ArrayList<ShoppingList>();
-        listView.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener()
-        {
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long i)
-            {
-
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> arg0) {
-                // TODO Auto-generated method stub
-
-            }
-        });
+        int listCount = listView.getCount();
+//        listView.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener()
+//        {
+//            public void onItemSelected(AdapterView<?> parent, View view, int position, long i)
+//            {
+//                BrowserAdapter bla = (BrowserAdapter) parent.getAdapter();
+//                bla.setCheckedItem(item);
+//            }
+//
+//            @Override
+//            public void onNothingSelected(AdapterView<?> arg0) {
+//                // TODO Auto-generated method stub
+//
+//            }
+//        });
 
         // Assign adapter to ListView
         listView.setAdapter(mDataAdapter);
     }
+
+//    private class MyAdapter extends BaseAdapter{
+//        public HashMap<String,String> checked = new HashMap<String,String>();
+//
+//        public void setCheckedItem(int item) {
+//
+//
+//            if (checked.containsKey(String.valueOf(item))){
+//                checked.remove(String.valueOf(item));
+//            }
+//
+//            else {
+//                checked.put(String.valueOf(item), String.valueOf(item));
+//            }
+//        }
+//        public HashMap<String, String> getCheckedItems(){
+//            return checked;
+//        }
+//
+//        @Override
+//        public int getCount() {
+//            return 0;  //To change body of implemented methods use File | Settings | File Templates.
+//        }
+//
+//        public String getItem(int n){
+//            return null;
+//        }
+//
+//        @Override
+//        public long getItemId(int i) {
+//            return 0;  //To change body of implemented methods use File | Settings | File Templates.
+//        }
+//
+//        @Override
+//        public View getView(int i, View view, ViewGroup viewGroup) {
+//            return null;  //To change body of implemented methods use File | Settings | File Templates.
+//        }
+//    }
 
 //    private void checkButtonClick() {
 //
